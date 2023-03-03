3ec089eb4abb49d699a843d05035bfd3216f89ca
electimon <electimon@gmail.com>
2023-03-03 15:10:53 +0800

    sm4250-common: Reorganize and cleanup platform tree
    * media_codecs.xml and friends will be copied from
      the media HAL
    
    Signed-off-by: electimon <electimon@gmail.com>

--- PlatformConfig.mk
+++ PlatformConfig.mk
@@ -12,42 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Arch
 TARGET_ARCH_VARIANT := armv8-a
-TARGET_CPU_ABI := arm64-v8a
 TARGET_2ND_ARCH_VARIANT := armv8-a
 
-BOARD_KERNEL_BASE        := 0x00000000
-BOARD_KERNEL_PAGESIZE    := 4096
-BOARD_RAMDISK_OFFSET     := 0x01000000
-BOARD_DTB_OFFSET         := 0x01f00000
-BOARD_MKBOOTIMG_ARGS += --dtb_offset $(BOARD_DTB_OFFSET)
-
-TARGET_RECOVERY_FSTAB ?= $(PLATFORM_COMMON_PATH)/rootdir/vendor/etc/fstab.qcom
-
-# Kernel
-BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=4e00000.dwc3
-TARGET_KERNEL_SOURCE := kernel/motorola/msm-4.19
-BOARD_KERNEL_IMAGE_NAME := Image.gz
-
-# SELinux
-BOARD_VENDOR_SEPOLICY_DIRS += $(PLATFORM_COMMON_PATH)/sepolicy_platform
-
-# Boot Image Header
-BOARD_BOOT_HEADER_VERSION ?= 2
-BOARD_INCLUDE_DTB_IN_BOOTIMG := true
-
-# Build a separate vendor.img
-TARGET_COPY_OUT_VENDOR := vendor
-BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
-
-# Build product image
-TARGET_COPY_OUT_PRODUCT := product
-BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := ext4
-
-# This platform has a metadata partition: declare this
-# to create a mount point for it
-BOARD_USES_METADATA_PARTITION := true
-
+# AVB
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
 BOARD_AVB_VBMETA_SYSTEM := system
@@ -56,13 +25,24 @@ BOARD_AVB_VBMETA_SYSTEM_ALGORITHM ?= SHA256_RSA2048
 BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
 BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX_LOCATION := 1
 
-# DTBO Recovery
-BOARD_INCLUDE_RECOVERY_DTBO := true
+# FSTab
+TARGET_RECOVERY_FSTAB ?= $(PLATFORM_COMMON_PATH)/rootdir/vendor/etc/fstab.qcom
 
-# USB
-SOONG_CONFIG_MOTO_COMMON_USB_USB_CONTROLLER_NAME := 4e00000
+# Kernel
+BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=4e00000.dwc3
+BOARD_KERNEL_IMAGE_NAME := Image.gz
 
 # SELinux
 BOARD_USE_ENFORCING_SELINUX := true
+BOARD_VENDOR_SEPOLICY_DIRS += $(PLATFORM_COMMON_PATH)/sepolicy
+
+# Partitions
+TARGET_COPY_OUT_PRODUCT := product
+TARGET_COPY_OUT_VENDOR := vendor
+BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+
+# USB
+SOONG_CONFIG_MOTO_COMMON_USB_USB_CONTROLLER_NAME := 4e00000
 
 include device/motorola/common/CommonConfig.mk
--- platform.mk
+++ platform.mk
@@ -21,43 +21,9 @@ TARGET_KERNEL_VERSION := 4.19
 PRODUCT_PLATFORM_MOT := true
 TARGET_BOARD_PLATFORM := $(BENGAL)
 
-# Kernel Headers
-#ifeq ($(TARGET_BUILDS_AOSP),true)
-PRODUCT_VENDOR_KERNEL_HEADERS := device/motorola/sm4250-common-kernel/kernel-headers
-#endif
-
-# Rootdir Path
-MOTOROLA_ROOT := $(PLATFORM_COMMON_PATH)/rootdir
-
-# Camera
-TARGET_USES_64BIT_CAMERA := true
-
-# Dynamic Partitions: Enable DP
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
 # A/B support
 AB_OTA_UPDATER := true
 PRODUCT_SHIPPING_API_LEVEL := 29
-
-# A/B OTA dexopt package
-PRODUCT_PACKAGES += \
-    otapreopt_script
-
-# A/B OTA dexopt update_engine hookup
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
-
-# A/B related packages
-PRODUCT_PACKAGES += \
-    update_engine \
-    update_engine_client \
-    update_engine_sideload \
-    update_verifier \
-    bootctrl.sm4250-common \
-    bootctrl.sm4250-common.recovery
 
 AB_OTA_PARTITIONS += \
     boot \
@@ -68,92 +34,54 @@ AB_OTA_PARTITIONS += \
     vbmeta \
     vbmeta_system
 
-# Dynamic Partitions: build fastbootd
-PRODUCT_PACKAGES += \
-    fastbootd
-
-# Treble
-# Include vndk/vndk-sp/ll-ndk modules
-PRODUCT_PACKAGES += \
-    vndk_package
-
-# Device Specific Permissions
-PRODUCT_COPY_FILES += \
-     frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.gyroscope.xml \
-     frameworks/native/data/etc/android.hardware.sensor.barometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.barometer.xml \
-     frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepcounter.xml \
-     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepdetector.xml
-
 # Audio
 PRODUCT_COPY_FILES += \
-    $(MOTOROLA_ROOT)/vendor/etc/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
-    $(MOTOROLA_ROOT)/vendor/etc/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
-    $(MOTOROLA_ROOT)/vendor/etc/audio_policy_configuration_bluetooth_legacy_hal.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration_bluetooth_legacy_hal.xml \
-    $(MOTOROLA_ROOT)/vendor/firmware/aw882xx_pid_1852_acf.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw882xx_pid_1852_acf.bin \
-    $(MOTOROLA_ROOT)/vendor/firmware/aw882xx_pid_2113_acf.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw882xx_pid_2113_acf.bin
+    $(PLATFORM_COMMON_PATH)/rootdir/vendor/etc/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
+    $(PLATFORM_COMMON_PATH)/rootdir/vendor/etc/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
+    $(PLATFORM_COMMON_PATH)/rootdir/vendor/firmware/aw882xx_pid_1852_acf.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw882xx_pid_1852_acf.bin \
+    $(PLATFORM_COMMON_PATH)/rootdir/vendor/firmware/aw882xx_pid_2113_acf.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw882xx_pid_2113_acf.bin
 
-# Media
+# Dynamic Partitions
+TARGET_USES_DYNAMIC_PARTITIONS := true
+
+# Fingerprint
 PRODUCT_COPY_FILES += \
-    $(MOTOROLA_ROOT)/vendor/etc/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
-    $(MOTOROLA_ROOT)/vendor/etc/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
-    $(MOTOROLA_ROOT)/vendor/etc/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
-    $(MOTOROLA_ROOT)/vendor/etc/media_profiles_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml
+    $(PLATFORM_COMMON_PATH)/rootdir/vendor/usr/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
+    $(PLATFORM_COMMON_PATH)/rootdir/vendor/usr/idc/uinput-fpc.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/uinput-fpc.idc
 
-# Qualcom WiFi Overlay
-PRODUCT_COPY_FILES += \
-    $(MOTOROLA_ROOT)/vendor/etc/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
-    $(MOTOROLA_ROOT)/vendor/etc/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf
-
-# Qualcom WiFi Configuration
-PRODUCT_COPY_FILES += \
-    $(MOTOROLA_ROOT)/vendor/firmware/wlan/qca_cld/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/firmware/wlan/qca_cld/WCNSS_qcom_cfg.ini
-
-# MSM IRQ Balancer configuration file
-PRODUCT_COPY_FILES += \
-    $(MOTOROLA_ROOT)/vendor/etc/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
-
-# Sensors
-# hardware.ssc.so links against display mappers, of which
-# the interface libraries are explicitly included here:
-PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0-service.multihal \
-    vendor.qti.hardware.display.mapper@1.1.vendor \
-    vendor.qti.hardware.display.mapper@3.0.vendor
-
-# QCOM Bluetooth
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.qcom.bluetooth.soc=cherokee
-
-# Legacy BT property (will be removed in S)
-PRODUCT_PROPERTY_OVERRIDES += \
-    vendor.qcom.bluetooth.soc=cherokee
+TARGET_USES_FPC_FINGERPRINT := true
 
 # Gatekeeper
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.gatekeeper.disable_spu=true
 
-# FPSensor Gestures
+# Media
 PRODUCT_COPY_FILES += \
-    $(MOTOROLA_ROOT)/vendor/usr/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
-    $(MOTOROLA_ROOT)/vendor/usr/idc/uinput-fpc.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/uinput-fpc.idc
+    $(PLATFORM_COMMON_PATH)/rootdir/vendor/etc/media_profiles_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml
 
-# Fingerprint
-TARGET_USES_FPC_FINGERPRINT := true
+# MSM IRQ Balancer configuration file
+PRODUCT_COPY_FILES += \
+    $(PLATFORM_COMMON_PATH)/rootdir/vendor/etc/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
 
 # Power
 PRODUCT_COPY_FILES += \
-    $(MOTOROLA_ROOT)/vendor/etc/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+    $(PLATFORM_COMMON_PATH)/rootdir/vendor/etc/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 PRODUCT_USES_PIXEL_POWER_HAL := true
 
-# USB
-PRODUCT_USES_PIXEL_USB_HAL := true
+# QCOM Bluetooth
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.qcom.bluetooth.soc=cherokee
+
+# Qualcomm WiFi Configuration
+PRODUCT_COPY_FILES += \
+    $(PLATFORM_COMMON_PATH)/rootdir/vendor/etc/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
+    $(PLATFORM_COMMON_PATH)/rootdir/vendor/etc/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
+    $(PLATFORM_COMMON_PATH)/rootdir/vendor/firmware/wlan/qca_cld/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/firmware/wlan/qca_cld/WCNSS_qcom_cfg.ini
 
 # Telephony
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.default_network=10,10
 
 $(call inherit-product, device/motorola/common/common.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 $(call inherit-product, vendor/motorola/sm4250-common/sm4250-common-vendor.mk)
